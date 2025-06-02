1a7eb0881648ba820623e88478a0e1d48ac724b9
Yuki Takei <yuki@weseek.co.jp>
2025-06-02 11:01:11 +0000

    add orval configuration file

---
+++ orval.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  apiv1: {
+    input: {
+      target: 'https://docs.growi.org/openapi-spec-apiv1.json',
+    },
+    output: {
+      mode: 'split',
+      target: './src/generated/v1/',
+      client: 'axios',
+      prettier: false, // Disable because we're using Biome
+      override: {
+        mutator: {
+          path: './src/api/axios-instance.ts',
+          name: 'customInstance',
+        },
+      },
+    },
+  },
+  apiv3: {
+    input: {
+      target: 'https://docs.growi.org/openapi-spec-apiv3.json',
+    },
+    output: {
+      mode: 'split',
+      target: './src/generated/v3/',
+      client: 'axios',
+      prettier: false, // Disable because we're using Biome
+      override: {
+        mutator: {
+          path: './src/api/axios-instance.ts',
+          name: 'customInstance',
+        },
+      },
+    },
+  },
+}
