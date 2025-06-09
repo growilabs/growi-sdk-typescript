52467f8f19dc604960acc87192c0a37394ea2fdb
Yuki Takei <yuki@weseek.co.jp>
2025-06-09 13:05:16 +0000

    clean index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
-// Export the main AXIOS_INSTANCE for configuration
-export { AXIOS_INSTANCE } from './utils/axios-instance.js';
+export { DEFAULT_AXIOS_INSTANCE } from './utils/axios-instance.js';
 
 export type * as Apiv1Types from './apiv1/index.js';
 export type * as Apiv3Types from './apiv3/index.js';
-
-export { default as apiv1Client } from './apiv1/index.js';
-export { default as apiv3Client } from './apiv3/index.js';
