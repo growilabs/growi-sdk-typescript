1a3d9bae84b98d3c24e28100b24ed8c12626490d
Shun Miyazawa <m0zurillex@gmail.com>
2025-09-29 17:22:38 +0000

    feat(index): export axiosInstanceManager from axios-instance-manager module

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { AXIOS_DEFAULT } from './utils/axios-default-instance.js';
+export { axiosInstanceManager } from './utils/axios-instance-manager.js';
 
 export type * as Apiv1Types from './apiv1/index.js';
 export type * as Apiv3Types from './apiv3/index.js';
