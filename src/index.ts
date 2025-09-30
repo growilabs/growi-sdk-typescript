203bb093a24010fff8b54ffba7d37024a3339199
Shun Miyazawa <m0zurillex@gmail.com>
2025-09-30 14:34:34 +0000

    rm AXIOS_DEFAULT (2)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-export { AXIOS_DEFAULT } from './utils/axios-default-instance.js';
 export { axiosInstanceManager } from './utils/axios-instance-manager.js';
 
 export type * as Apiv1Types from './apiv1/index.js';
