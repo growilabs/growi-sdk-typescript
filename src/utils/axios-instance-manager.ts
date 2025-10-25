15c76f5a62d05e9968a2ad3db2dbdfe0c0b37bd0
Ryu Sato <ryu@weseek.co.jp>
2025-10-25 14:12:50 +0000

    validate input parameters in addAxiosInstance method

--- src/utils/axios-instance-manager.ts
+++ src/utils/axios-instance-manager.ts
@@ -12,6 +12,10 @@ class AxiosInstanceManager {
    * @param config The configuration for the Axios instance.
    */
   addAxiosInstance(config: { appName: string; baseURL: string; token: string; authorizationHeader: string | undefined }) {
+    if (config.appName.length === 0) throw new Error('appName must be a non-empty string');
+    if (config.baseURL.length === 0) throw new Error('baseURL must be a non-empty string');
+    if (config.token.length === 0) throw new Error('token must be a non-empty string');
+
     const axiosInstance = Axios.create({
       baseURL: config.baseURL,
     });
@@ -21,7 +25,7 @@ class AxiosInstanceManager {
       axiosInstance.defaults.headers.common.Authorization = config.authorizationHeader;
       axiosInstance.defaults.headers['X-GROWI-ACCESS-TOKEN'] = config.token;
     } else {
-    axiosInstance.defaults.headers.common.Authorization = `Bearer ${config.token}`;
+      axiosInstance.defaults.headers.common.Authorization = `Bearer ${config.token}`;
     }
 
     this.instances.set(config.appName, axiosInstance);
